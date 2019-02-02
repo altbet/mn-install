e0b009a255134350d76610b03b6dce6345609b23
Altbet.io <42963544+altbet@users.noreply.github.com>
2019-02-03 00:22:24 +0100

    v1.3.0.1 update

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
-echo -e "Updating vps"
+echo -e "Downloading altbet v1.3.0.1"
 cd /tmp
 wget -N https://github.com/altbet/abet/releases/download/v1.3.0.1/altbet-v1.3.0.1-ubu1604.tar.gz
 tar xvzf altbet-v1.3.0.1-ubu1604.tar.gz
+clear
+echo -e "Updating altbet to the latest version"
 systemctl stop altbet
 mv altbetd altbet-cli /usr/local/bin
 rm altbet-v1.3.0.1-ubu1604.tar.gz
+clear
+echo -e "Preparing altbet for latest bootstrap"
 cd /root/.altbet
 mv wallet.dat walletold.dat
 rm -r {blocks,database,fee_estimates.dat,mnpayments.dat,altbetd.pid,budget.dat,db.log,peers.dat,chainstate,debug.log,mncache.dat$
@@ -12,7 +16,10 @@ wget -q https://github.com/altbet/bootstraps/releases/download/v1.3.0.1-bootstra
 unzip bootstrap.zip
 rm bootstrap.zip >/dev/null 2>&1
 cd - >/dev/null 2>&1
+clear
+echo -e "Starting altbet deamon, please be patient"
 systemctl start altbet
 clear
 sleep 10
 altbet-cli getinfo
+echo -e "Update completed, have a nice day :)"
