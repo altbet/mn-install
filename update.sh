d57847d90ca22f4d4546f4f5bac8f39f969006f3
Altbet.io <42963544+altbet@users.noreply.github.com>
2019-02-19 01:43:04 +0100

    v1.3.1.0 update

--- update.sh
+++ update.sh
@@ -1,23 +1,23 @@
-echo -e "Downloading altbet v1.3.0.1"
+echo -e "Downloading Altbet v1.3.1.0"
 cd /tmp
-wget -N https://github.com/altbet/abet/releases/download/v1.3.0.1/altbet-v1.3.0.1-ubu1604.tar.gz
-tar xvzf altbet-v1.3.0.1-ubu1604.tar.gz
+wget -N https://github.com/altbet/abet/releases/download/v1.3.1.0/altbet-v1.3.1.0-ubu1604.tar.gz
+tar xvzf altbet-v1.3.1.0-ubu1604.tar.gz
 clear
-echo -e "Updating altbet to the latest version"
+echo -e "Updating Altbet to the latest version"
 systemctl stop altbet
 mv altbetd altbet-cli /usr/local/bin
-rm altbet-v1.3.0.1-ubu1604.tar.gz
+rm altbet-v1.3.1.0-ubu1604.tar.gz
 clear
-echo -e "Preparing altbet for latest bootstrap"
+echo -e "Preparing Altbet for latest bootstrap"
 cd /root/.altbet
-mv wallet.dat walletold.dat
+mv wallet.dat walletold1.dat
 rm -r {budget.dat,db.log,debug.log,fee_estimates.dat,mnpayments.dat,mncache.dat,peers.dat,wallet.dat,blocks,chainstate} >/dev/null 2>&1
-wget -q https://github.com/altbet/bootstraps/releases/download/166215/bootstrap.zip -O bootstrap.zip
+wget -q https://github.com/altbet/bootstraps/releases/download/178000/bootstrap.zip -O bootstrap.zip
 unzip bootstrap.zip
 rm bootstrap.zip >/dev/null 2>&1
 cd - >/dev/null 2>&1
 clear
-echo -e "Starting altbet deamon, please be patient"
+echo -e "Starting Altbet daemon, please be patient"
 systemctl start altbet
 clear
 sleep 10
