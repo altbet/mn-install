ec9c7f2e102756483ecb85e996930fd6ae98b0fa
Altbet.io <42963544+altbet@users.noreply.github.com>
2019-04-13 02:15:04 +0200

    v.2.0.0.1

--- update.sh
+++ update.sh
@@ -1,26 +1,29 @@
-echo -e "This script is currently out of date. Download the mn-install.sh script from the official Altbet github!"
-#echo -e "Downloading Altbet v1.3.1.0"
-#cd /tmp
-#wget -N https://github.com/altbet/abet/releases/download/v1.3.1.0/altbet-v1.3.1.0-ubu1604.tar.gz
-#tar xvzf altbet-v1.3.1.0-ubu1604.tar.gz
-#clear
-#echo -e "Updating Altbet to the latest version"
-#systemctl stop altbet
-#mv altbetd altbet-cli /usr/local/bin
-#rm altbet-v1.3.1.0-ubu1604.tar.gz
-#clear
-#echo -e "Preparing Altbet for latest bootstrap"
-#cd /root/.altbet
-#mv wallet.dat walletold1.dat
-#rm -r {budget.dat,db.log,debug.log,fee_estimates.dat,mnpayments.dat,mncache.dat,peers.dat,wallet.dat,blocks,chainstate} >/dev/null 2>&1
-#wget -q https://github.com/altbet/bootstraps/releases/download/229842/bootstrap.zip -O bootstrap.zip
-#unzip bootstrap.zip
-#rm bootstrap.zip >/dev/null 2>&1
-#cd - >/dev/null 2>&1
-#clear
-#echo -e "Starting Altbet daemon, please be patient"
-#systemctl start altbet
-#clear
-#sleep 10
-#altbet-cli getinfo
-#echo -e "Update completed, have a nice day :)"
+echo -e "Downloading Altbet v2.0.0.1"
+cd /tmp
+wget -N https://github.com/altbet/abet/releases/download/v.2.0.0.1/altbet-v2.0.0.1-ubu1604.tar.gz
+tar xvzf altbet-v2.0.0.1-ubu1604.tar.gz
+clear
+echo -e "Updating Altbet to the latest version"
+systemctl stop altbet
+mv altbetd altbet-cli /usr/local/bin
+rm altbet-v2.0.0.1-ubu1604.tar.gz
+clear
+echo -e "Preparing Altbet for latest bootstrap"
+cd /root/.altbet
+mv wallet.dat walletold1.dat
+rm -r {budget.dat,fee_estimates.dat,peers.dat,chainstate,sporks,backups,db.log,mncache.dat,wallet.dat,blocks,debug.log,mnpayments.dat,zerocoin} >/dev/null 2>&1
+wget -q https://github.com/altbet/bootstraps/releases/download/242100/bootstrap.zip -O bootstrap.zip
+unzip bootstrap.zip
+rm bootstrap.zip >/dev/null 2>&1
+cd - >/dev/null 2>&1
+clear
+echo -e "Starting Altbet daemon, please be patient"
+systemctl start altbet
+clear
+sleep 5
+altbet-cli addnode 8.9.36.49:2238 onetry
+sleep 2
+altbet-cli addnode 45.32.87.111:2238 onetry
+sleep 2
+altbet-cli getinfo
+echo -e "Update completed, have a nice day :)"
