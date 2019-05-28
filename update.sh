d08428ab4a4aeea4d071eec7f05fd2b9e5605543
Altbet.io <42963544+altbet@users.noreply.github.com>
2019-05-28 13:29:26 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ echo -e "Preparing Altbet for latest bootstrap"
 cd /root/.altbet
 mv wallet.dat walletold1.dat
 rm -r {banlist.dat,budget.dat,fee_estimates.dat,peers.dat,chainstate,sporks,backups,db.log,mncache.dat,wallet.dat,blocks,debug.log,mnpayments.dat,zerocoin} >/dev/null 2>&1
-wget -q https://github.com/altbet/bootstraps/releases/download/305916/bootstrap.zip -O bootstrap.zip
+wget -q https://github.com/altbet-coin/bootstraps/releases/download/5-26-2019/5-26-2019.zip -O bootstrap.zip
 unzip bootstrap.zip
 rm bootstrap.zip >/dev/null 2>&1
 cd - >/dev/null 2>&1
@@ -21,13 +21,21 @@ echo -e "Starting Altbet daemon, please be patient"
 systemctl start altbet
 clear
 sleep 3
-altbet-cli addnode 188.40.182.64:2238 onetry
+altbet-cli addnode 185.206.146.209:2238 onetry
 sleep 1
-altbet-cli addnode 95.217.54.157:2238 onetry
+altbet-cli addnode 185.206.147.210:2238 onetry
 sleep 1
-altbet-cli addnode 136.243.63.208:2238 onetry
+altbet-cli addnode 185.206.144.217:2238 onetry
 sleep 1
-altbet-cli addnode 188.213.166.16:2238 onetry
+altbet-cli addnode 185.141.61.104:2238 onetry
+sleep 1
+altbet-cli addnode 173.199.70.37:2238 onetry
+sleep 1
+altbet-cli addnode 95.216.79.247:2238 onetry
+sleep 1
+altbet-cli addnode 188.40.177.100:2238 onetry
+sleep 1
+altbet-cli addnode 46.4.178.72:2238 onetry
 sleep 1
 altbet-cli getinfo
 echo -e "Update completed, have a nice day :)"
